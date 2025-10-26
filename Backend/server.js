32b337dae38f8bb5b8598d98b5ee7bbdbfca09ff
kushwaha7886 <kushwaha7886@gmail.com>
2025-10-26 12:36:02 +0530

    Initial commit

---
+++ Backend/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send(' Server is running ')
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
